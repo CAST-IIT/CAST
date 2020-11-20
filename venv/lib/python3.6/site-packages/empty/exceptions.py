e1d985e2625212229c2cc06502eeb91e663ede3b
Vedaanti Baliga <vedaantibaliga@gmail.com>
2020-11-20 09:38:19 +0530

    Added gitignore and fixed forms.py

--- venv/lib/python3.6/site-packages/empty/exceptions.py
+++
@@ -1,9 +0,0 @@
-class NoExtensionException(Exception):
-    def __init__(self, e_name, *args, **kwargs):
-        super(NoExtensionException, self).__init__(
-            'No {} extension found'.format(e_name)
-        )
-
-
-class BlueprintException(Exception):
-    pass
