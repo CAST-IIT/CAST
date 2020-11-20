f2aee33591d6a5baa76147d92ce12e27874a4a1e
Vedaanti Baliga <vedaantibaliga@gmail.com>
2020-11-20 13:28:22 +0530

    Config changes

--- groundwater/config.py
+++ groundwater/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = 'd168651a2aa242e14428a991c42164ef'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root:your_password_here@localhost/groundwater'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:your_password_here@localhost/groundwater'
